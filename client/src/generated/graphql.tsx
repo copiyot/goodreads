8c02dca9b35840c1d0ba97fecb386d9aba8efb12
Castro Opiyo <castroopiyo@Castros-MacBook-Pro.local>
2023-04-14 11:48:06 +0300

    feat: add user query

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -133,6 +133,11 @@ export type RegisterMutationVariables = Exact<{
 
 export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'UserResponse', user?: { __typename?: 'User', id: number, username: string } | null, errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null } };
 
+export type BooksQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type BooksQuery = { __typename?: 'Query', books: Array<{ __typename?: 'Book', id: number, title: string, author: string, collection: string, coverImage: string }> };
+
 export type MeQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -255,6 +260,44 @@ export function useRegisterMutation(baseOptions?: Apollo.MutationHookOptions<Reg
 export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>;
 export type RegisterMutationResult = Apollo.MutationResult<RegisterMutation>;
 export type RegisterMutationOptions = Apollo.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
+export const BooksDocument = gql`
+    query Books {
+  books {
+    id
+    title
+    author
+    collection
+    coverImage
+  }
+}
+    `;
+
+/**
+ * __useBooksQuery__
+ *
+ * To run a query within a React component, call `useBooksQuery` and pass it any options that fit your needs.
+ * When your component renders, `useBooksQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useBooksQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useBooksQuery(baseOptions?: Apollo.QueryHookOptions<BooksQuery, BooksQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<BooksQuery, BooksQueryVariables>(BooksDocument, options);
+      }
+export function useBooksLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<BooksQuery, BooksQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<BooksQuery, BooksQueryVariables>(BooksDocument, options);
+        }
+export type BooksQueryHookResult = ReturnType<typeof useBooksQuery>;
+export type BooksLazyQueryHookResult = ReturnType<typeof useBooksLazyQuery>;
+export type BooksQueryResult = Apollo.QueryResult<BooksQuery, BooksQueryVariables>;
 export const MeDocument = gql`
     query Me {
   me {
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,19 @@
+import { useBooksQuery } from "../generated/graphql";
 import NavBar from "../components/NavBar";
 
 const Home = () => {
-  return <NavBar />;
+  const { data, loading, error } = useBooksQuery();
+
+  return (
+    <>
+      <NavBar />
+      {!data ? (
+        <div>Loading....</div>
+      ) : (
+        data.books.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
 };
 
 export default Home;
