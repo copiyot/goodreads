8301f30bfb42ed14d6c520475334d9c1ded4b9a2
Castro Opiyo <castroopiyo@Castros-MacBook-Pro.local>
2023-04-15 00:05:01 +0300

    feat: add books list

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,7 +4,7 @@ import { useBooksQuery } from "../generated/graphql";
 import NavBar from "../components/NavBar";
 
 const Home = () => {
-  const { data, loading, error } = useBooksQuery();
+  const { data } = useBooksQuery();
 
   return (
     <>
@@ -12,16 +12,63 @@ const Home = () => {
       {!data ? (
         <div>Loading....</div>
       ) : (
-        <div className="bg-gray-50 min-h-screen md:p-10 p-2">
+        <div className="bg-gray-100 min-h-screen md:p-10 p-2">
           <Link
             href="/create_book"
             className="bg-[#002D74] rounded-md text-white p-2 hover:scale-105 duration-300"
           >
             Add Book
           </Link>
-          {data.books.map((p) => (
-            <div key={p.id}>{p.title}</div>
-          ))}
+          <div className="text-center text-2xl font=[Poppins] text-[#002D74]">
+            Your Books
+          </div>
+          <div className="overflow-auto rounded-lg shadow mt-5">
+            <table className="w-full">
+              <thead className="bg-gray-50 border-b-2 border-gray-200">
+                <tr>
+                  <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                    No
+                  </th>
+                  <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                    Title
+                  </th>
+                  <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                    Author
+                  </th>
+                  <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                    Collection
+                  </th>
+                  <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                    Cover Image
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-100">
+                {data.books.map((book, index) => (
+                  <tr
+                    key={book.id}
+                    className={index % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                  >
+                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                      {++index}
+                    </td>
+                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                      {book.title}
+                    </td>
+                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                      {book.author}
+                    </td>
+                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                      {book.collection}
+                    </td>
+                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                      {book.coverImage}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       )}
     </>
